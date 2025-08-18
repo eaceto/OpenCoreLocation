f7a2acbd1a608281859974a955d6726bb90921c6
Ezequiel Aceto <ezequiel.aceto@gmail.com>
2025-08-18 13:04:51 +0200

    feat: Add cross-platform support including Linux

--- Package.swift
+++ Package.swift
@@ -6,8 +6,10 @@ import PackageDescription
 let package = Package(
     name: "OpenCoreLocation",
     platforms: [
-        .macOS(.v13),
-        .iOS(.v15)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
         .library(
